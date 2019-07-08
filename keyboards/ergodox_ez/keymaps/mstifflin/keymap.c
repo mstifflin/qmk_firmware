4a7331585fc83ef8f91cbf79ecb1dc8ca60efa00
Tiffany Lin <tifflin@uber.com>
2019-07-08 16:58:53 -0700

    Add equals and minus to base layer

--- keyboards/ergodox_ez/keymaps/mstifflin/keymap.c
+++ keyboards/ergodox_ez/keymaps/mstifflin/keymap.c
@@ -69,9 +69,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // Right hand
     TG(LAYER_NUM_PAD)  , KC_6          , KC_7          , KC_8          , KC_9          , KC_0          , KC_BSPACE     ,
-    _______            , KC_Y          , KC_U          , KC_I          , KC_O          , KC_P          , KC_BSLASH     ,
+    KC_MINUS           , KC_Y          , KC_U          , KC_I          , KC_O          , KC_P          , KC_BSLASH     ,
                          KC_H          , KC_J          , KC_K          , KC_L          , KC_SCOLON     , KC_QUOTE      ,
-    _______            , KC_N          , KC_M          , KC_COMMA      , KC_DOT        , KC_SLASH      , KC_LSHIFT     ,
+    KC_EQUAL           , KC_N          , KC_M          , KC_COMMA      , KC_DOT        , KC_SLASH      , KC_LSHIFT     ,
     KC_LBRACKET        , KC_RBRACKET   , _______       , _______       , KC_LCTRL      ,
 
     _______       , _______,
