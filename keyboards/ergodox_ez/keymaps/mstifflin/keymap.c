b54e5bcad1a393446b161ffce9e3bc9209b85bb4
Tiffany Lin <tifflin@uber.com>
2019-09-23 13:37:07 -0700

    Remove toggle for numpad
    I keep accidentally pressing that button

--- keyboards/ergodox_ez/keymaps/mstifflin/keymap.c
+++ keyboards/ergodox_ez/keymaps/mstifflin/keymap.c
@@ -63,11 +63,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                     KC_LGUI, MO(LAYER_NUM_PAD), KC_AUDIO_VOL_DOWN,
 
     // Right hand
-    TG(LAYER_NUM_PAD)  , KC_6          , KC_7          , KC_8          , KC_9          , KC_0          , KC_BSPACE     ,
-    KC_EQUAL           , KC_Y          , KC_U          , KC_I          , KC_O          , KC_P          , KC_BSLASH     ,
-                         KC_H          , KC_J          , KC_K          , KC_L          , KC_SCOLON     , KC_QUOTE      ,
-    KC_MINUS           , KC_N          , KC_M          , KC_COMMA      , KC_DOT        , KC_SLASH      , KC_LSHIFT     ,
-    KC_LBRACKET        , KC_RBRACKET   , _______       , _______       , KC_LCTRL      ,
+    _______    , KC_6          , KC_7          , KC_8          , KC_9          , KC_0          , KC_BSPACE     ,
+    KC_EQUAL   , KC_Y          , KC_U          , KC_I          , KC_O          , KC_P          , KC_BSLASH     ,
+                 KC_H          , KC_J          , KC_K          , KC_L          , KC_SCOLON     , KC_QUOTE      ,
+    KC_MINUS   , KC_N          , KC_M          , KC_COMMA      , KC_DOT        , KC_SLASH      , KC_LSHIFT     ,
+    KC_LBRACKET, KC_RBRACKET   , _______       , _______       , KC_LCTRL      ,
 
     _______       , _______,
     KC_MS_WH_UP   ,
