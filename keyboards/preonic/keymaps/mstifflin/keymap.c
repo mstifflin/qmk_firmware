97716f1ef79d51d59cd7ef4b8ad8c0348a48c569
Tiffany Lin <tifflin@uber.com>
2020-01-17 11:43:48 -0800

    [Preonic] Update media keys

--- keyboards/preonic/keymaps/mstifflin/keymap.c
+++ keyboards/preonic/keymaps/mstifflin/keymap.c
@@ -43,15 +43,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+-------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+-------|
- * | Ctrl | Alt  | GUI  | Alt  | GUI  |Spc/Lw|Spc/Rs| [    |   ]  | Play | Vol- | Vol+  |
+ * | Ctrl | Alt  | GUI  | Alt  | GUI  |Spc/Lw|Spc/Rs| Next | Play | Mute | Vol- | Vol+  |
  * `------------------------------------------------------------------------------------'
  */
 [_DEFAULT] = LAYOUT_preonic_grid( \
-  KC_GRV,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,               KC_6,               KC_7,       KC_8,    KC_9,    KC_0,              KC_BSPC,        \
-  KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,               KC_Y,               KC_U,       KC_I,    KC_O,    KC_P,              KC_BSLS,        \
-  CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,               KC_J,       KC_K,    KC_L,    KC_SCLN,           KC_QUOT,        \
-  KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_N,               KC_M,       KC_COMM, KC_DOT,  KC_SLSH,           KC_LSFT,        \
-  KC_LCTL,       KC_LALT, KC_LGUI, KC_LALT, KC_LGUI, LT(_LOWER, KC_SPC), LT(_RAISE, KC_SPC), KC_LBRC,    KC_RBRC, KC_MUTE, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP \
+  KC_GRV,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,               KC_6,               KC_7,    KC_8,    KC_9,    KC_0,              KC_BSPC,        \
+  KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,               KC_Y,               KC_U,    KC_I,    KC_O,    KC_P,              KC_BSLS,        \
+  CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,               KC_J,    KC_K,    KC_L,    KC_SCLN,           KC_QUOT,        \
+  KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_N,               KC_M,    KC_COMM, KC_DOT,  KC_SLSH,           KC_LSFT,        \
+  KC_LCTL,       KC_LALT, KC_LGUI, KC_LALT, KC_LGUI, LT(_LOWER, KC_SPC), LT(_RAISE, KC_SPC), KC_MPLY, KC_MNXT, KC_MUTE, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP \
 ),
 
 /* Lower
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, KC_LCBR, KC_MINUS, KC_EQUAL, KC_RCBR, KC_PIPE, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______,  _______,  KC_HOME, KC_END,  _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______,  KC_MNXT,  KC_VOLD, KC_VOLU, KC_MPLY \
+  _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, _______, _______ \
 ),
 
 /* Raise
