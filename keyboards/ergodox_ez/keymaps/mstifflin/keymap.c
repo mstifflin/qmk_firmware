ac28cbc2d89193070afbe6e46e97b21b9eca7b95
Tiffany Lin <tifflin@uber.com>
2020-08-06 12:08:41 -0700

    [Ergodox] Move esc, change how to toggle to secondary layer off of space

--- keyboards/ergodox_ez/keymaps/mstifflin/keymap.c
+++ keyboards/ergodox_ez/keymaps/mstifflin/keymap.c
@@ -53,26 +53,26 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT_ergodox(
     // Left hand
-    KC_GRV        , KC_1       , KC_2          , KC_3          , KC_4          , KC_5          , _______       ,
-    KC_TAB        , KC_Q       , KC_W          , KC_E          , KC_R          , KC_T          , KC_LBRACKET   ,
-    CTL_T(KC_ESC) , KC_A       , KC_S          , KC_D          , KC_F          , KC_G          ,
-    KC_LSHIFT     , CTL_T(KC_Z), KC_X          , KC_C          , KC_V          , KC_B          , KC_RBRACKET   ,
-    KC_LCTRL      , KC_LALT    , _______       , KC_LCTRL      , KC_LALT       ,
+    KC_ESC   , KC_1   , KC_2          , KC_3          , KC_4          , KC_5          , _______       ,
+    KC_TAB   , KC_Q   , KC_W          , KC_E          , KC_R          , KC_T          , KC_LBRACKET   ,
+    KC_LCTRL , KC_A   , KC_S          , KC_D          , KC_F          , KC_G          ,
+    KC_LSHIFT, KC_Z,    KC_X          , KC_C          , KC_V          , KC_B          , KC_RBRACKET   ,
+    KC_LCTRL , KC_LALT, _______       , KC_LCTRL      , KC_LALT       ,
 
                                                                              MO(LAYER_NUM_PAD),          KC_SPACE,
                                                                                                   KC_AUDIO_VOL_UP,
                                                                     KC_LGUI, MO(LAYER_MISC_CTRL), KC_AUDIO_VOL_DOWN,
 
     // Right hand
-    _______    , KC_6          , KC_7          , KC_8          , KC_9          , KC_0          , KC_BSPACE     ,
-    KC_EQUAL   , KC_Y          , KC_U          , KC_I          , KC_O          , KC_P          , KC_BSLASH     ,
-                 KC_H          , KC_J          , KC_K          , KC_L          , KC_SCOLON     , KC_QUOTE      ,
-    KC_MINUS   , KC_N          , KC_M          , KC_COMMA      , KC_DOT        , KC_SLASH      , KC_LSHIFT     ,
-    KC_LBRACKET, KC_RBRACKET   , _______       , _______       , KC_LCTRL      ,
+    _______    , KC_6   , KC_7          , KC_8          , KC_9          , KC_0          , KC_BSPACE     ,
+    KC_EQUAL   , KC_Y   , KC_U          , KC_I          , KC_O          , KC_P          , KC_BSLASH     ,
+                 KC_H   , KC_J          , KC_K          , KC_L          , KC_SCOLON     , KC_QUOTE      ,
+    KC_MINUS   , KC_N   , KC_M          , KC_COMMA      , KC_DOT        , KC_SLASH      , KC_LSHIFT     ,
+    KC_GRV,      _______, _______       , _______       , KC_LCTRL      ,
 
     KC_MPRV, KC_CAPSLOCK,
     KC_MNXT,
-    KC_MPLY, MO(LAYER_MOUSE_CTRL), LT(LAYER_MISC_CTRL, KC_SPACE)
+    KC_MPLY, MO(LAYER_MISC_CTRL), KC_SPACE
   ),
 
   [LAYER_WINDOWS] = LAYOUT_ergodox(
