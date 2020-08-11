da0fa75d4fd2e6da3920e6c5f6ebb42f439fb100
Tiffany Lin <tifflin@uber.com>
2020-08-10 18:16:16 -0700

    [Preonic] Move esc and grave keybindings to be similar to ergodox

--- keyboards/preonic/keymaps/mstifflin/keymap.c
+++ keyboards/preonic/keymaps/mstifflin/keymap.c
@@ -1,17 +1,4 @@
-/* Copyright 2015-2017 Jack Humbert
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+/* Tiffany Lin's Preonic Layout
  */
 
 #include QMK_KEYBOARD_H
@@ -36,23 +23,44 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Default
  * ,------------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp  |
+ * | Esc  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp  |
  * |------+------+------+------+------+------+------+------+------+------+------+-------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |   \   |
  * |------+------+------+------+------+-------------+------+------+------+------+-------|
- * |EscCtl|   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  '    |
+ * | Ctl  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  '    |
  * |------+------+------+------+------+------|------+------+------+------+------+-------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+-------|
- * | Ctrl | Alt  | GUI  | Alt  | GUI  |Raise |Spc/Rs| Play | Next | Mute | Vol- | Vol+  |
+ * | Ctrl | Alt  | GUI  | Alt  | GUI  |Space |Spc/Rs| Grave| Play | Next | Vol- | Vol+  |
  * `------------------------------------------------------------------------------------'
  */
 [_DEFAULT] = LAYOUT_preonic_grid( \
-  KC_GRV,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,               KC_7,    KC_8,    KC_9,    KC_0,              KC_BSPC,        \
-  KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,               KC_U,    KC_I,    KC_O,    KC_P,              KC_BSLS,        \
-  CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,               KC_J,    KC_K,    KC_L,    KC_SCLN,           KC_QUOT,        \
-  KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,               KC_M,    KC_COMM, KC_DOT,  KC_SLSH,           KC_LSFT,        \
-  KC_LCTL,       KC_LALT, KC_LGUI, KC_LALT, KC_LGUI, MO(_RAISE), LT(_RAISE, KC_SPC), KC_MPLY, KC_MNXT, KC_MUTE, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP \
+  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,               KC_7,   KC_8,    KC_9,    KC_0,              KC_BSPC,        \
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,               KC_U,   KC_I,    KC_O,    KC_P,              KC_BSLS,        \
+  KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,               KC_J,   KC_K,    KC_L,    KC_SCLN,           KC_QUOT,        \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,               KC_M,   KC_COMM, KC_DOT,  KC_SLSH,           KC_LSFT,        \
+  KC_LCTL, KC_LALT, KC_LGUI, KC_LALT, KC_LGUI, KC_SPC, LT(_RAISE, KC_SPC), KC_GRV, KC_MPLY, KC_MNXT, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP \
+),
+
+/* Raise
+ * ,-----------------------------------------------------------------------------------.
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |   [  |   -  |   =  |   ]  |      |      |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Caps |      |      |      |      |      | Left | Down |  Up  |Right |      |Enter |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      |      | Home | PgUp | PgDwn| End  |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      |      |      |      | WinLO|
+ * `-----------------------------------------------------------------------------------'
+ */
+[_RAISE] = LAYOUT_preonic_grid( \
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,     KC_F9,    KC_F10,   KC_F11,  KC_F12,  \
+  _______, _______, _______, _______, _______, _______, KC_LBRC, KC_MINUS,  KC_EQUAL, KC_RBRC,  _______, _______,  \
+  KC_CAPS, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN,   KC_UP,    KC_RIGHT, _______, KC_ENTER, \
+  _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDOWN, KC_PGUP,  KC_END,   _______, _______,  \
+  _______, _______, _______, _______, _______, _______, _______, _______,   _______,  _______,  _______, TG(_WINDOWS)   \
 ),
 
 /* Lower
@@ -74,27 +82,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   RESET,   _______, _______, _______, _______, _______, KC_MPRV, KC_MPLY, _______, KC_MNXT, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
-),
-
-/* Raise
- * ,-----------------------------------------------------------------------------------.
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |   [  |   -  |   =  |   ]  |      |      |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Caps |      |      |      |      |      | Left | Down |  Up  |Right |      |Enter |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |      |      |      |      |
- * `-----------------------------------------------------------------------------------'
- */
-[_RAISE] = LAYOUT_preonic_grid( \
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,     KC_F9,    KC_F10,   KC_F11,  KC_F12,  \
-  _______, _______, _______, _______, _______, _______, KC_LBRC, KC_MINUS,  KC_EQUAL, KC_RBRC,  _______, _______,  \
-  KC_CAPS, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN,   KC_UP,    KC_RIGHT, _______, KC_ENTER, \
-  _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDOWN, KC_PGUP,  KC_END,   _______, _______,  \
-  _______, _______, _______, _______, _______, _______, _______, _______,   _______,  _______,  _______, TG(_WINDOWS)   \
 ),
 
 /* Windows
@@ -177,12 +164,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
               backlight_step();
             #endif
             #ifdef __AVR__
-            PORTE &= ~(1<<6);
+            writePinLow(E6);
             #endif
           } else {
             unregister_code(KC_RSFT);
             #ifdef __AVR__
-            PORTE |= (1<<6);
+            writePinHigh(E6);
             #endif
           }
           return false;
@@ -223,7 +210,7 @@ void encoder_update_user(uint8_t index, bool clockwise) {
   }
 }
 
-void dip_update(uint8_t index, bool active) {
+void dip_switch_update_user(uint8_t index, bool active) {
   switch (index) {
     case 0:
       if (active) {
@@ -237,27 +224,29 @@ void dip_update(uint8_t index, bool active) {
         muse_mode = true;
       } else {
         muse_mode = false;
-        #ifdef AUDIO_ENABLE
-          stop_all_notes();
-        #endif
       }
-   }
+  }
 }
 
 void matrix_scan_user(void) {
-  #ifdef AUDIO_ENABLE
-    if (muse_mode) {
-      if (muse_counter == 0) {
-        uint8_t muse_note = muse_offset + SCALE[muse_clock_pulse()];
-        if (muse_note != last_muse_note) {
-          stop_note(compute_freq_for_midi_note(last_muse_note));
-          play_note(compute_freq_for_midi_note(muse_note), 0xF);
-          last_muse_note = muse_note;
-        }
+#ifdef AUDIO_ENABLE
+  if (muse_mode) {
+    if (muse_counter == 0) {
+      uint8_t muse_note = muse_offset + SCALE[muse_clock_pulse()];
+      if (muse_note != last_muse_note) {
+        stop_note(compute_freq_for_midi_note(last_muse_note));
+        play_note(compute_freq_for_midi_note(muse_note), 0xF);
+        last_muse_note = muse_note;
       }
-      muse_counter = (muse_counter + 1) % muse_tempo;
     }
-  #endif
+    muse_counter = (muse_counter + 1) % muse_tempo;
+  } else {
+    if (muse_counter) {
+      stop_all_notes();
+      muse_counter = 0;
+    }
+  }
+#endif
 }
 
 bool music_mask_user(uint16_t keycode) {
